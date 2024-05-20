e5585c194733d5d775c470ddf39260869d60d57d
madhu dharani <cmdharani2014@gmail.com>
2024-05-20 19:19:15 +0530

    Queue Consumer Console App

---
+++ AzureQueueConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+ String AzureConnectionString = @"DefaultEndpointsProtocol=https;AccountName=mdstorage;AccountKey=Su5D1e3CnGaspuq2dxyt1IPu7W5rqwxz8069QqouVqhvjaYEDuxOUEvXtwr2KivgfDd1MK9WZ1xa+AStJ4RJgA==;EndpointSuffix=core.windows.net";
+
+QueueClient queue = new QueueClient(AzureConnectionString, "attendee-emails");
+
+if(await queue.ExistsAsync())
+{
+    QueueProperties properties = await queue.GetPropertiesAsync();
+    for(int i=0;i< properties.ApproximateMessagesCount;i++) 
+    {
+        string message = await RetrieveNextMessage();
+        Console.WriteLine(message);
+    }
+}
+
+async Task<string> RetrieveNextMessage()
+{
+    QueueMessage[] retrivedMessage = await queue.ReceiveMessagesAsync(1);
+    var data = Convert.FromBase64String(retrivedMessage[0].Body.ToString());
+    string theMessage=Encoding.UTF8.GetString(data);
+
+    await queue.DeleteMessageAsync(retrivedMessage[0].MessageId, retrivedMessage[0].PopReceipt);
+
+    return theMessage;
+}
